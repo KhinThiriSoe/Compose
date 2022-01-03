f5b0f66903598381e82df26168921df9c22b97c6
Khin <khinthirisoe.mdy@gmail.com>
2022-01-03 13:54:20 +0700

    styling text

--- app/src/main/java/com/khin/composecourse/MainActivity.kt
+++ app/src/main/java/com/khin/composecourse/MainActivity.kt
@@ -8,8 +8,6 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Card
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -19,8 +17,16 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.khin.composecourse.ui.theme.ComposeCourseTheme
@@ -42,6 +48,17 @@ class MainActivity : ComponentActivity() {
                 Text(text = "Thiri")
             }*/
 
+            val fontFamily = FontFamily(
+                Font(R.font.inter_thin, FontWeight.Thin),
+                Font(R.font.inter_light, FontWeight.Light),
+                Font(R.font.inter_extralight, FontWeight.ExtraLight),
+                Font(R.font.inter_regular, FontWeight.Normal),
+                Font(R.font.inter_black, FontWeight.Black),
+                Font(R.font.inter_medium, FontWeight.Medium),
+                Font(R.font.inter_bold, FontWeight.Bold),
+                Font(R.font.inter_semibold, FontWeight.SemiBold),
+                Font(R.font.inter_extrabold, FontWeight.ExtraBold)
+            )
 
             ComposeCourseTheme {
                 val painter = painterResource(id = R.drawable.ic_beach_access)
@@ -53,6 +70,40 @@ class MainActivity : ComponentActivity() {
                         .padding(16.dp)
                 ) {
                     ImageCard(painter = painter, contentDescription = description, title = title)
+                }
+                Box(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .background(Color(0xFF898989))
+                ) {
+                    Text(
+                        color = Color.White,
+                        fontSize = 30.sp,
+                        fontFamily = fontFamily,
+                        fontWeight = FontWeight.Bold,
+                        fontStyle = FontStyle.Italic,
+                        textAlign = TextAlign.Center,
+                        textDecoration = TextDecoration.Underline,
+                        text = buildAnnotatedString {
+                            withStyle(
+                                style = SpanStyle(
+                                    color = Color.Magenta,
+                                    fontSize = 50.sp
+                                )
+                            ) {
+                                append("J")
+                            }
+                            append("etpack ")
+                            withStyle(
+                                style = SpanStyle(
+                                    color = Color.Magenta,
+                                    fontSize = 50.sp
+                                )
+                            ) {
+                                append("C")
+                            }
+                            append("ompose")
+                        })
                 }
             }
         }
